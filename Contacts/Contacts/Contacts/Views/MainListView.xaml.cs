503c92884709e62f00997d2cbc87a6b50b7dd0b7
EugenSeko <satyagrah108@gmail.com>
2021-10-06 12:19:36 +0300

    Added simple ListView and simple Navigation

--- Contacts/Contacts/Contacts/Views/MainListView.xaml.cs
+++ Contacts/Contacts/Contacts/Views/MainListView.xaml.cs
@@ -12,9 +12,25 @@ namespace Contacts.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MainListView : ContentPage
     {
+        public string[] Items {get;set;}
+
         public MainListView()
         {
             InitializeComponent();
+         string[]items = new string[24];
+            for (int i = 0; i < 24; i++)
+            {
+                items[i] = $"Item {i}";
+            }
+            Items = items;
+            this.BindingContext = this;
+            backButton.Clicked += BackButton_Click;
+
+
+        }
+        private async void BackButton_Click(object sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync();
         }
     }
 }
--- Contacts/Contacts/Contacts/Views/SignUpView.xaml.cs
+++ Contacts/Contacts/Contacts/Views/SignUpView.xaml.cs
@@ -15,6 +15,16 @@ namespace Contacts.Views
         public SignUpView()
         {
             InitializeComponent();
+            backButton.Clicked += BackButton_Click;
+            signUpButton.Clicked += SignUpButton_Click;
+        }
+        private async void BackButton_Click(object sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
+        private async void SignUpButton_Click(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new MainListView());
         }
     }
 }
